2be9b4813ea6c8c6dfff2475b2313b71429ffe4a
Sebastian Reverso <sebastian.reverso@sovos.com>
2019-06-25 10:20:37 -0500

    2 way data binding example

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
 const InputElement = () => {
-    return (<input placeholder="Enter some Text" />);
+
+    const [inputText, setInputText] = useState("");
+    const [historyList, setHistoryList] = useState([]);
+
+    return <div><input
+        onChange={(e) => {
+            setInputText(e.target.value);
+            setHistoryList([...historyList,e.target.value]);
+        }}
+        placeholder="Enter some Text" /><br/>
+        {inputText}
+        <hr/><br/>
+        <ul>
+            {historyList.map((x) => {
+                return <div>{x}</div>
+            })}
+        </ul>
+    </div>
 };
 
 export default InputElement;
